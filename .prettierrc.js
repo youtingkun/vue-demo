637b1010e53ef672f1df416c92f52fee9c676b21
unknown <10816@ps.com>
2020-12-11 11:12:15 +0800

    feat: commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     printWidth: 80, // 每行代码长度（默认80）
     tabWidth: 2, // 每个tab相当于多少个空格（默认2）
-    useTabs: false, // 是否使用tab进行缩进（默认false）
+    useTabs: true, // 是否使用tab进行缩进（默认false）
     singleQuote: true, // 使用单引号（默认false）
     semi: true, // 声明结尾使用分号(默认true)
     trailingComma: 'all', // 多行使用拖尾逗号（默认none）
