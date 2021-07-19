6a29289651ca5b99b9129868155d46d4cd7313c6
youtingkun <youtingkun@techsailing.com>
2021-07-19 20:49:51 +0800

    feat: gallery

---
+++ src/views/wheel/gallery/GalleryUpload.vue
@@ -0,0 +1,86 @@
+<template>
+	<el-dialog title="批量上传" :visible.sync="dialogVisible">
+		<div class="all-photo">
+			<div class="item">
+				<el-upload class="avatar-uploader" action="action" multiple :http-request="uploadFile">
+					<i class="el-icon-plus avatar-uploader-icon"></i>
+					<div>添加</div>
+				</el-upload>
+			</div>
+			<div class="item"></div>
+		</div>
+
+		<span slot="footer" class="dialog-footer">
+			<el-button @click="dialogVisible = false">取 消</el-button>
+			<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+		</span>
+	</el-dialog>
+</template>
+
+<script>
+export default {
+	name: '',
+	components: {},
+	props: {},
+	data() {
+		return {
+			dialogVisible: true,
+		};
+	},
+	computed: {},
+	watch: {},
+	methods: {
+		openDialog() {
+			this.dialogVisible = true;
+		},
+		uploadFile(param) {
+			debugger;
+		},
+	},
+	created() {},
+	mounted() {},
+	beforeCreate() {},
+	beforeMount() {},
+	beforeUpdate() {},
+	updated() {},
+	beforeDestroy() {},
+	destroyed() {},
+	activated() {},
+};
+</script>
+<style lang="scss">
+.all-photo {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	.item {
+		margin: 8px;
+	}
+}
+.avatar-uploader .el-upload {
+	border: 1px dashed #d9d9d9;
+	border-radius: 6px;
+	cursor: pointer;
+	position: relative;
+	overflow: hidden;
+	width: 135px;
+	height: 135px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-content: center;
+}
+.avatar-uploader .el-upload:hover {
+	border-color: #409eff;
+	color: #409eff;
+}
+.avatar-uploader-icon {
+	font-size: 28px;
+}
+.el-dialog__header {
+	border-bottom: 1px solid #d9d9d9;
+}
+.el-dialog__footer {
+	border-top: 1px solid #d9d9d9;
+}
+</style>
--- src/views/wheel/gallery/index.vue
+++ src/views/wheel/gallery/index.vue
@@ -2,33 +2,62 @@
 	<div class="app-container">
 		<div class="gallery-wrap">
 			<div class="left">
-				<div class="group-list">
-					<el-button class="item">全部</el-button>
-					<el-button class="item">分组1</el-button>
-					<el-button class="item">分组2</el-button>
-				</div>
-				<el-button class="group-add" icon="el-icon-plus">添加分组</el-button>
+				<el-menu class="group-list" default-active="2" @select="handleGroupSelect">
+					<el-menu-item index="100" class="menu-item">全部（100）</el-menu-item>
+					<el-menu-item v-for="(item, i) in groupList" :key="item.id" :index="item.id" class="menu-item">
+						<div class="name">{{ `${item.name}(${item.number})` }}</div>
+						<div>
+							<i class="el-icon-edit-outline action" @click="handleEditGroupName(item.name, i)"> </i>
+							<i class="el-icon-delete action" @click="handleDeleteGroup(item)"></i>
+						</div>
+					</el-menu-item>
+				</el-menu>
+				<el-button class="group-add" icon="el-icon-plus" @click="handleAddGroup">添加分组</el-button>
 			</div>
 			<div class="right">
 				<div class="header">
-					<el-button type="primary">上传</el-button>
+					<el-upload
+						action="action"
+						multiple
+						:http-request="handleUploadFile"
+						:show-file-list="false"
+						accept="image/png, image/jpeg"
+					>
+						<el-button type="primary" icon="el-icon-plus">上传</el-button>
+					</el-upload>
+
 					<div class="search">
 						图片名称：<el-input class="input" v-model="searchForm.name"></el-input
-						><el-button>查询</el-button>
+						><el-button @click="handelSearch">查询</el-button>
 					</div>
 				</div>
 				<div class="body">
 					<el-card shadow="never">
 						<div slot="header">
-							<el-checkbox label="全选" v-model="isAllSelect"></el-checkbox>
-							<el-button type="text" style="margin-left: 10px">批量删除</el-button>
-							<el-button type="text">归档分类</el-button>
+							<el-checkbox label="全选" v-model="isAllSelect" @change="handleAllSelect"></el-checkbox>
+							<el-button
+								type="text"
+								style="margin-left: 10px"
+								@click="handleMultipleDeletePhoto"
+								:disabled="!this.selectList.length > 0"
+								>批量删除</el-button
+							>
+							<el-button
+								type="text"
+								@click="handleMultipleRankPhoto"
+								:disabled="!this.selectList.length > 0"
+								>归档分类</el-button
+							>
 						</div>
-						<div class="img-item-wrap">
-							<div class="img-item" v-for="item in dataList" :key="item">
+						<div class="img-item-wrap" v-loading="dataListLoading">
+							<div class="img-item" v-for="(item, index) in dataList" :key="item.id">
 								<img class="img" src="www.baidu.com" />
 								<div>
-									<el-checkbox label="这是名称"></el-checkbox>
+									<el-checkbox
+										v-model="dataList[index].isSelect"
+										@change="handleCheckboxChange"
+										label="这是名称"
+									></el-checkbox>
 								</div>
 							</div>
 						</div>
@@ -36,18 +65,55 @@
 				</div>
 				<div class="footer">
 					<el-pagination
-						@size-change="handleSizeChange"
-						@current-change="handleCurrentChange"
-						:current-page="1"
+						background
+						@size-change="getDataList"
+						@current-change="getDataList"
+						:current-page.sync="searchForm.pageNumber"
 						:page-sizes="[10, 20, 50, 100]"
-						:page-size="100"
+						:page-size.sync="searchForm.pageSize"
 						layout="total, sizes, prev, pager, next, jumper"
-						:total="400"
+						:total="searchForm.total"
 					>
 					</el-pagination>
 				</div>
 			</div>
 		</div>
+		<el-dialog :title="dialogTitle" :visible.sync="dialogVisible" width="300px" :close-on-click-modal="false">
+			<el-form>
+				<el-form-item label="">
+					<el-input v-model="groupItemInputValue"></el-input>
+				</el-form-item>
+			</el-form>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="dialogVisible = false">取 消</el-button>
+				<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+			</span>
+		</el-dialog>
+		<el-dialog title="归档分类" :visible.sync="rankDialogVisible" width="700px" :close-on-click-modal="false">
+			<el-radio-group v-model="rankRadio">
+				<el-radio :label="3">分组1</el-radio>
+				<el-radio :label="6">分组2</el-radio>
+				<el-radio :label="9">分组3</el-radio>
+			</el-radio-group>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="rankDialogVisible = false">取 消</el-button>
+				<el-button type="primary" @click="rankDialogVisible = false">确 定</el-button>
+			</span>
+		</el-dialog>
+		<el-dialog
+			title="上传进度"
+			:visible.sync="progressDialogVisible"
+			width="700px"
+			:close-on-click-modal="false"
+			:close-on-press-escape="false"
+			:show-close="false"
+		>
+			{{ this.successUploadFileLength }}{{ this.uploadFileLength }}
+			<el-progress
+				v-if="this.uploadFileLength != 0"
+				:percentage="(this.successUploadFileLength / this.uploadFileLength) * 100"
+			></el-progress>
+		</el-dialog>
 	</div>
 </template>
 
@@ -58,20 +124,139 @@ export default {
 	props: {},
 	data() {
 		return {
+			progressDialogVisible: false,
+			rankDialogVisible: false,
+			dialogTitle: '新增',
+			dialogVisible: false,
 			searchForm: {
 				name: '',
+				pageSize: 10,
+				pageNumber: 1,
+				total: 100,
 			},
 			isAllSelect: false,
 			selectList: [],
-			groupList: [{ name: '', number: 123 }],
-			dataList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+			groupList: [
+				{ name: '分组1', number: 100, isEdit: false, id: '1' },
+				{ name: '分组2', number: 10, isEdit: false, id: '2' },
+				{ name: '分组3', number: 20, isEdit: false, id: '3' },
+			],
+			rankRadio: '',
+			dataList: [
+				{ id: 1, isSelect: false },
+				{ id: 2, isSelect: false },
+				{ id: 3, isSelect: false },
+				{ id: 4, isSelect: false },
+				{ id: 5, isSelect: false },
+				{ id: 6, isSelect: false },
+				{ id: 7, isSelect: false },
+				{ id: 8, isSelect: false },
+				{ id: 9, isSelect: false },
+				{ id: 10, isSelect: false },
+			],
+			dataListLoading: false,
+			groupItemInputValue: '',
+			uploadFileLength: 0,
+			successUploadFileLength: 0,
 		};
 	},
 	computed: {},
 	watch: {},
 	methods: {
+		handelSearch() {
+			this.searchForm.pageSize = 10;
+			this.searchForm.pageNumber = 1;
+			this.getDataList();
+		},
+		getDataList() {
+			console.log('获取数据', this.searchForm);
+		},
+		handleGroupSelect(index) {
+			console.log(index);
+		},
+		handleUploadFile(param) {
+			console.log(param);
+			this.uploadFileLength++;
+			this.progressDialogVisible = true;
+			console.log('uploadFileLength', this.uploadFileLength);
+			setTimeout(() => {
+				this.successUploadFileLength++;
+				if (this.successUploadFileLength === this.uploadFileLength) {
+					this.progressDialogVisible = false;
+					this.successUploadFileLength = 0;
+					this.uploadFileLength = 0;
+				}
+			}, this.uploadFileLength * 3000);
+		},
 		handleSizeChange() {},
 		handleCurrentChange() {},
+		handleEditGroupName(name, index) {
+			this.groupItemInputValue = name;
+			this.dialogVisible = true;
+			this.dialogTitle = '编辑分组';
+		},
+		handleAddGroup() {
+			this.dialogVisible = true;
+			this.dialogTitle = '新增分组';
+		},
+		handleEditGroupNameSure() {},
+		handleDeleteGroup() {
+			this.$confirm('确认删除分组？', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning',
+			})
+				.then(() => {
+					this.$message({
+						type: 'success',
+						message: '删除成功!',
+					});
+				})
+				.catch(() => {
+					this.$message({
+						type: 'info',
+						message: '已取消删除',
+					});
+				});
+		},
+		handleMultipleDeletePhoto() {
+			this.$confirm('确认删除所选图片？', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning',
+			})
+				.then(() => {
+					this.$message({
+						type: 'success',
+						message: '删除成功!',
+					});
+				})
+				.catch(() => {
+					this.$message({
+						type: 'info',
+						message: '已取消删除',
+					});
+				});
+		},
+		handleMultipleRankPhoto() {
+			this.rankDialogVisible = true;
+		},
+		handleRankCheckChange() {},
+		handleCheckboxChange() {
+			console.log('this.dataList', this.dataList);
+		},
+		handleAllSelect(value) {
+			if (value) {
+				this.dataList.map((item, index) => {
+					this.dataList[index].isSelect = true;
+				});
+			} else {
+				this.dataList.map((item, index) => {
+					this.dataList[index].isSelect = false;
+				});
+			}
+			console.log('this.dataList', this.dataList);
+		},
 	},
 	created() {},
 	mounted() {},
@@ -100,10 +285,38 @@ export default {
 			display: flex;
 			flex-direction: column;
 			padding: 16px 8px;
-			.item {
+			.menu-item {
 				height: 48px;
 				margin: 8px;
-				border: none;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				.name {
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+				}
+			}
+			.item {
+				margin: 8px;
+				display: flex;
+				flex-direction: row;
+				line-height: 48px;
+				align-items: center;
+				.button {
+					text-align: left;
+					flex: 1;
+					border: none;
+				}
+				.button:hover {
+					cursor: pointer;
+					color: #409eff;
+					background-color: #f4f4f9;
+				}
+				.action:hover {
+					cursor: pointer;
+					color: #409eff;
+				}
 			}
 		}
 		.group-add {
